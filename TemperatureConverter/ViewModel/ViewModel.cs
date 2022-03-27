7acbbcb4c9737312fd01c30fd437ce7007d1c7b7
Elian Bollue <elian.bollue@student.ucll.be>
2022-03-27 19:56:45 +0200

    wa cells

---
+++ TemperatureConverter/ViewModel/ViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cells;
+using Model;
+
+namespace ViewModel
+{
+    public class ConvertViewModel
+    {
+        public ConvertViewModel()
+        {
+            this.TemperatureInKelvin = new Cell<double>();
+
+            this.Kelvin = new TemperatureScaleViewModel(this, new KelvingTemperatureScale());
+            this.Celsius = new TemperatureScaleViewModel(this, new CelsiusTemperatureScale());
+            this.Fahrenheit = new TemperatureScaleViewModel(this, new FahrenheitTemperatureScale());
+        }
+
+        public Cell<double> TemperatureInKelvin { get; }
+
+        public TemperatureScaleViewModel Kelvin { get; }
+
+        public TemperatureScaleViewModel Celsius { get; }
+
+        public TemperatureScaleViewModel Fahrenheit { get; }
+
+        public IEnumerable<TemperatureScaleViewModel> Scales
+        {
+            get
+            {
+                yield return Celsius;
+                yield return Fahrenheit;
+                yield return Kelvin;
+            }
+        }
+    }
+
+    public class TemperatureScaleViewModel : INotifyPropertyChanged
+    {
+        private readonly ConvertViewModel parent;
+
+        private readonly ITemperatureScale temperatureScale;
+
+        public TemperatureScaleViewModel(ConvertViewModel parent, ITemperatureScale temperatureScale)
+        {
+            this.parent = parent;
+            this.temperatureScale = temperatureScale;
+
+            this.parent.TemperatureInKelvin.PropertyChanged += (sender, args) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Temperature)));
+        }
+
+        public string Name => temperatureScale.Name;
+
+        public double Temperature
+        {
+            get
+            {
+                return temperatureScale.ConvertFromKelvin(parent.TemperatureInKelvin.Value);
+            }
+            set
+            {
+                parent.TemperatureInKelvin.Value = temperatureScale.ConvertToKelvin(value);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
